cee6f457491cec07b40add23ca73333174d948af
fishystock <215427190+fishystock@users.noreply.github.com>
2025-06-08 22:14:57 -0400

    Add gitsint.py

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # gitsint
- Quickly pull information from Git repositories.
+### Installation
+
+```
+$ git clone https://github.com/fishystock/gitsint
+$ python3 -m venv venv
+$ source venv/bin/activate
+$ pip install -r requirements.txt
+```
+
+### Usage
+
+```
+$ python3 gitsint.py --help
+usage: gitsint.py [-h] repo_path
+
+Extract emails and timezones from a local GitHub repository
+
+positional arguments:
+  repo_path   Path to the local GitHub repository
+
+options:
+  -h, --help  show this help message and exit
+```
+
+```
+$ python3 gitsint.py /tmp/python3-ransomware
+Fetching information from /tmp/python3-ransomware. This may take awhile on large repositories.
++--------------------------+-------------+
+| Email                    |   Timezones |
++==========================+=============+
+| ferrogiacomo26@gmail.com |       +0100 |
++--------------------------+-------------+
+```
---
+++ gitsint.py
@@ -0,0 +1,33 @@
+import argparse
+from collections import defaultdict
+from git import Repo
+from tabulate import tabulate
+
+def extract_emails_and_timezones(repo_path):
+    repo = Repo(repo_path)
+    data = defaultdict(set)
+    seen_commits = set()
+
+    for ref in repo.references:
+        for commit in repo.iter_commits(ref):
+            if commit.hexsha not in seen_commits:
+                seen_commits.add(commit.hexsha)
+                email = commit.author.email
+                tz = commit.authored_datetime.strftime('%z')
+                data[email].add(tz)
+
+    return data
+
+def main():
+    parser = argparse.ArgumentParser(description='Extract emails and timezones from a local GitHub repository')
+    parser.add_argument('repo_path', type=str, help='Path to the local GitHub repository')
+    args = parser.parse_args()
+
+    print(f'Fetching information from {args.repo_path}. This may take awhile on large repositories.')
+
+    data = extract_emails_and_timezones(args.repo_path)
+    table = [(email, ', '.join(sorted(timezones))) for email, timezones in sorted(data.items())]
+    print(tabulate(table, headers=["Email", "Timezones"], tablefmt="grid"))
+
+if __name__ == '__main__':
+    main()
